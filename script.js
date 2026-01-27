1ac2f2da148f0a76c18e0b926aef7714a877aeb0
ahmedqureshi6120-eng <ahmedqureshi6120@gmail.com>
2026-01-27 21:15:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var menu = document.getElementById("courseMenu");
+
+function show() {
+  menu.style.display = "block";
+}
+
+function hide() {
+  menu.style.display = "none";
+}
+
+const texts = ["Web Development", "Graphic Designing", "Data Science", "Cyber Security"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+  if (count === texts.length) {
+    count = 0; // loop back
+  }
+
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.typing-text').textContent = letter;
+
+  if (letter.length === currentText.length) {
+    setTimeout(() => {
+      index = 0;
+      count++;
+      type();
+    }, 1500); // pause before next word
+  } else {
+    setTimeout(type, 150); // typing speed
+  }
+})();
