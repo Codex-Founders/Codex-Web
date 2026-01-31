f4b8e29decc484911d7c9f0db10d16c587c01e45
Mudassir Shaikh <mudassirakram30@gmail.com>
2026-01-31 20:23:32 +0500

    Changes in JAVA

--- script.js
+++ script.js
@@ -36,3 +36,44 @@ let letter = '';
 })()
 
 
+var Name = document.getElementById("Fname").value;
+var FatherName = document.getElementById("Fathername").value;
+var Email = document.getElementById("email").value;
+var Phone = document.getElementById("phone").value;
+var address = document.getElementById("address").value;
+      
+
+function validateForm() {
+
+  var Name = document.getElementById("Fname").value;
+  var FatherName = document.getElementById("Fathername").value;
+  var Email = document.getElementById("email").value;
+  var Phone = document.getElementById("phone").value;
+  var address = document.getElementById("address").value;
+
+  if (
+    Name === "" ||
+    FatherName === "" ||
+    Email === "" ||
+    Phone === "" ||
+    address === ""
+  ) {
+    alert("Please fill all the fields.");
+    return false; 
+  }
+
+  alert("Form submitted successfully!");
+
+  var reset = document.getElementById("myForm");
+  reset.reset();
+
+  return false;
+}
+
+
+
+// var cardName = document.getElementById("Cname").value;
+// var CfatherName = document.getElementById("Cfathername").value;
+// var cardNumber = document.getElementById("Cnumber").value;
+// var Caddress = document.getElementById("Caddress").value;
+// var Cphone = document.getElementById("Cphone").value;
