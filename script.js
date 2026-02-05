8ddda60badb3685fa715174e3e124d137879b992
ahmedqureshi6120-eng <ahmedqureshi6120@gmail.com>
2026-02-05 23:32:06 +0500

    responsive

--- script.js
+++ script.js
@@ -88,3 +88,12 @@ if (downloadBtn) {
     });
   });
 }
+
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
+  });
+
