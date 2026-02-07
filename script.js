2b6482a9146e36592b2ffb103d79b245bc6eebce
ahmedqureshi6120-eng <ahmedqureshi6120@gmail.com>
2026-02-07 22:15:54 +0500

    updated

--- script.js
+++ script.js
@@ -89,11 +89,3 @@ if (downloadBtn) {
   });
 }
 
-  const hamburger = document.querySelector(".hamburger");
-  const navLinks = document.querySelector(".nav-links");
-
-  hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-    hamburger.classList.toggle("active");
-  });
-
